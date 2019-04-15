6ba4cfcc2a92f02fa6dcbfe98c172f287e9c2898
dylan <dewol103@mail.chapman.edu>
2019-04-14 18:28:44 -0700

    newsapi with pd data frame

--- news_test.py
+++ news_test.py
@@ -5,7 +5,7 @@ from newsapi.articles import Articles
 a = Articles(API_KEY = apikey)
 data = a.get(source = "the-new-york-times", sort_by = 'top')
 
-print (data) ## raw news data
+#print (data) ## raw news data
 
 ## -----------------------------------------------------------
 import numpy
@@ -15,14 +15,4 @@ data = pd.concat([data.drop(['articles'], axis=1), data['articles'].apply(pd.Ser
 
 data.head()
 
-from newsapi.sources import Sources
-s = Sources(API_KEY=apikey)
-
-# return all available sources
-sources = s.get()
-# convert to dataframe and drop status column
-sources = data = pd.DataFrame.from_dict(sources).drop('status', axis=1)
-# take the array column 'sources' and spread it across multiple columns
-sources = pd.concat([sources.drop(['sources'], axis=1),
-                     sources['sources'].apply(pd.Series)], axis=1).drop('urlsToLogos', axis=1)
-sources.tail()
+data.drop(columns=['status', 'source','sortBy','author','url','urlToImage','publishedAt'])
